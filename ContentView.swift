1e8057f54be561e32ff55284e7a8487b7a5d55af
sangambansode45 <bansodesangam23@gmail.com>
2025-02-24 22:14:24 +0530

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RegisterLoginApp
+//
+//  Created by Apple on 24/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+     LoginView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RegisterLoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterLoginAppApp.swift
+//  RegisterLoginApp
+//
+//  Created by Apple on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct RegisterLoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
